78555728ba4eb80fac39a82b2ed06d55460cbba9
Taoudi <Taoudi128@hotmail.com>
2020-01-20 18:37:25 +0100

    :thinking:

--- Lab1/partOne.py
+++ Lab1/partOne.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
-mA = [1.0, 0, 5]
-mB = [-1.0, 0.0]
-sigmaA = 0.5
-sigmaB = 0.5
+mA = [2.0, 1.0]
+mB = [-2.0, -1.0]
+sigmaA = 0.1
+sigmaB = 0.1
 
 
 def generateInputMatrix():
@@ -15,7 +15,7 @@ def generateInputMatrix():
     classB1 = np.array(np.random.randint(0, n, n) * np.array([sigmaB + mB[0]] * n))
     classB2 = np.array(np.random.randint(0, n, n) * np.array([sigmaB + mB[1]] * n))
 
-    classX = np.array([np.concatenate((classA1, classB1)), np.concatenate((classA2, classB2)), np.array([1] * (n * 2))])
+    classX = np.array([np.array([1] * (n * 2)), np.concatenate((classA1, classB1)), np.concatenate((classA2, classB2))])
     classW = np.random.normal(0, 0.01, classX.shape[0])
     print(classW)
     classAT = np.array([1] * n)
@@ -27,7 +27,7 @@ def generateInputMatrix():
     return classX, classT, classW
 
 
-def plot(a1, a2,b1,b2):
+def plot(a1, a2, b1, b2):
     plt.scatter(a1, a2, color="red")
     plt.scatter(b1, b2, color="blue")
 
@@ -59,10 +59,10 @@ def delta_learning(X, T, W):
         old_W = W
         changeW = delta(old_W, X, T)
         W = old_W - changeW
+        print(W)
         # if np.isnan[W[0]] or np.isnan(W[1]):
         # return old_W
-        print(W)
-        print("---------------")
+
         if converge_check(old_W, changeW):
             converged = True
     if not converged:
@@ -75,11 +75,17 @@ classX, classT, classW = generateInputMatrix()
 
 W = delta_learning(classX, classT, classW)
 
+
 def plotLine(W):
-    plt.ylim(top=55, bottom=-5)
+    plt.ylim(top=100, bottom=-100)
     x = np.linspace(-100, 100, 200)
-    y = (W[0] * x + W[1])/W[2]
-    plt.plot(x, y, color='green', label="Seperable line from weights")
+    b = W[0]
+    y = (W[1]*x + W[2])
+
+    #k = -(b / W[2]) / (b / W[1])
+    #m = -b/W[2]
+    #y = k*x +m
+    plt.plot(x, y, color='green', label="Decision Boundary")
     plt.legend(loc="upper right")
     plt.xlabel("X-feature")
     plt.ylabel("Y-feature")
