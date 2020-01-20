e6b30fb7f1675f1c13072f023013c32ed0ff5775
Kevin Ammouri <kammouri@kth.se>
2020-01-20 14:11:28 +0100

    sec

--- Lab1/partOne.py
+++ Lab1/partOne.py
@@ -2,32 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 100
-mA = [70, 80]
-mB = [-100, -90]
+mA = [1.0, 0.5]
+mB = [-1.0, 0.0]
 sigmaA = 0.5
 sigmaB = 0.5
 
 def generateInputMatrix():
     classA1 = np.array(np.random.randint(0, n, n) * np.array([sigmaA + mA[0]]*n))
     classAT = np.array(np.random.randint(0, n, n) * np.array([sigmaA + mA[1]]*n))
-
+    classA = np.array([classA1, classAT])
+    
     classB1 = np.array(np.random.randint(0, n, n) * np.array([sigmaB + mB[0]]*n))
     classBT = np.array(np.random.randint(0, n, n) * np.array([sigmaB + mB[1]]*n))
-
+    classB = np.array([classB1, classBT])
+    
     classT = np.concatenate((classAT,classBT))
-    #classT = classAT + classBT
     classX = np.array([np.concatenate((classA1, classB1)),   np.array([1]*(n*2))])
-
     classW = np.array([np.random.normal(0, 1), 1])
 
+    plot(classA, classB)
+    
     return classX, classT, classW
 
 def plot(dataset1, dataset2):
     plt.scatter(dataset1[0], dataset1[1], color="red")
     plt.scatter(dataset2[0], dataset2[1], color="blue")
-    plt.show()
 
-def delta(W, X, T, eta=0.001):
+def delta(W, X, T, eta=0.000001):
     """
     @param  W - Weight matrix
     @param  X - Input matrix
@@ -36,7 +37,7 @@ def delta(W, X, T, eta=0.001):
     @return - Delta Matrix
     """
 
-    return -eta * (W @ X - T)@ np.transpose(X)
+    return -eta * (W@X - T)@np.transpose(X)
 
 classX, classT, classW = generateInputMatrix()
 
@@ -46,14 +47,15 @@ def perceptron_learning(X, T, W):
     while not converged and i <= 100:
         i+=1
         print(W)
+        print("---------------")
         old_W = W
-        W = delta(old_W, X, T)
+        W = delta(old_W, X, T,0.000001)
         if np.array_equal(old_W, W):
             converged = True
     return W
 
 
-#W = perceptron_learning(classX, classT, classW)
+W = perceptron_learning(classX, classT, classW)
 #print(W)
 #W = np.array([1,1,1])
 #T = np.array([1]*100)
@@ -65,3 +67,9 @@ def perceptron_learning(X, T, W):
 
 #print(np.max(classB[0]))
 #print(np.max(classB[1]))
+
+x = classX[0]
+y = W[0]*x + W[1]
+plt.plot(x, y, '-r', label='y=2x+1')
+plt.grid()
+plt.show()
