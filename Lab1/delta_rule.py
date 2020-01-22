d1c16798d96a40a01a91074082546beef270a561
Taoudi <taoudi128@gmail.com>
2020-01-22 13:50:22 +0100

    Convergence check and bias

--- Lab1/delta_rule.py
+++ Lab1/delta_rule.py
@@ -8,31 +8,20 @@ sigmaA = 0.5
 sigmaB = 0.5
 eta = 0.0001
 
-def generateIOWMatrix(useBias=True):
-    return
-
 
 def generateInputMatrix(useBias=True):
     np.random.seed(100)
     classA1 = np.array(np.random.normal(2, sigmaA, n) * sigmaA + mA[0])
-    classA2 = np.array(np.random.normal(3, sigmaA, n) * sigmaA + mA[1])
-    classB1 = np.array(np.random.normal(7, sigmaB, n) * sigmaB + mB[0])
-    classB2 = np.array(np.random.normal(8, sigmaB, n) * sigmaB + mB[1])
+    classA2 = np.array(np.random.normal(2, sigmaA, n) * sigmaA + mA[1])
+    classB1 = np.array(np.random.normal(2, sigmaB, n) * sigmaB + mB[0])
+    classB2 = np.array(np.random.normal(2, sigmaB, n) * sigmaB + mB[1])
 
-    # classA1 = np.arange(1, (n + 1), 1)
-    # classA2 = np.arange(1, (n + 1), 1)
-    # classB1 = np.arange(-(n + 1), -1, 1)
-    # classB2 = np.arange(-(n + 1), -1, 1)
+    if useBias:
+        classX = np.array([np.concatenate((classA1, classB1)), np.concatenate((classA2, classB2)), np.array([1] * (n * 2))])
+    else:
+        classX = np.array([np.concatenate((classA1, classB1)), np.concatenate((classA2, classB2))])
 
-    # classA1 = np.array([0])
-    # classA2 = np.array([1])
-    # classB1= np.array([0])
-    # classB2 = np.array([2])
-
-    classX = np.array([np.concatenate((classA1, classB1)), np.concatenate((classA2, classB2)), np.array([1] * (n * 2))])
-    classW = np.random.normal(0, 0.01, classX.shape[0])
-    classAT = np.array([1] * n)  # Random ClassA1,ClassA2 -> Eps 1 eller -1
-    classBT = np.array([0] * n)  # Random
+    classW = np.random.normal(0, 5, classX.shape[0])
     classT = np.concatenate(([1] * n, [-1] * n))
 
     plot(classA1, classA2, classB1, classB2)
@@ -62,7 +51,7 @@ def converge_check(old, new, percentage=0.00001):
 
 
 def sum_square(X, W, T):
-    return np.sum((T - W @ X) ** 2)/2
+    return np.sum((T - W @ X) ** 2) / 2
 
 
 def sequential_delta_learning(X, T, W):
@@ -73,14 +62,12 @@ def sequential_delta_learning(X, T, W):
         i += 1
         old_W = W
         old_error = sum_square(X, old_W, T)
-        print(old_error)
         for j, x in enumerate(X.T):
             W += eta * ((T[j] - W @ x) * x)
         error = sum_square(X, W, T)
         error_values.append(error)
-        iterations.append(i*n*2)
+        iterations.append(i * n * 2)
         if converge_check(old_error, error):
-            print(W)
             return W, error_values, iterations
 
 
@@ -96,12 +83,11 @@ def delta_learning(X, T, W):
         changeW = delta(old_W, X, T)
         W = old_W + changeW
         old_error = sum_square(X, old_W, T)
-        print(old_error)
         error = sum_square(X, W, T)
         error_values.append(error)
+        print(error)
         iterations.append(i)
         if converge_check(old_error, error):
-            print(W)
             return W, error_values, iterations
 
 
@@ -116,13 +102,19 @@ def plotIters(errors, iters):
     plt.show()
 
 
-def plotLine(W):
+def plotLine(W, bias=True):
     plt.ylim(top=50, bottom=-10)
-    x = np.linspace(-10, 10, 200)
-    b = W[2]
-    k = -(b / W[1]) / (b / W[0])
-    m = -b / W[1]
-    y = k * x + m
+    y = []
+    x = np.linspace(-100, 100, 200)
+    if bias:
+        b = W[2]
+        k = -(b / W[1]) / (b / W[0])
+        m = -b / W[1]
+        y = k * x + m
+    else:
+        k = (W[0])/W[1]
+        y = k * x
+
     plt.plot(x, y, color='green', label="Decision Boundary")
     plt.legend(loc="upper right")
     plt.xlabel("X-feature")
