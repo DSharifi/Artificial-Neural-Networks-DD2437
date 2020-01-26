79fb8ec5f5defeaa11639502233ac217c7e21e2d
Kevin Ammouri <kammouri@kth.se>
2020-01-26 19:23:34 +0100

    updated version

--- Lab1/2layer.py
+++ Lab1/2layer.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # NETWORK SETTINGS
-n_epochs = 30000
-hidden_nodes = 16
+n_epochs = 20000
+hidden_nodes = 10
 features = 2  # input_nodes, for 3.2.1-3.2.2
 eta = 0.001
 output_nodes = 1
@@ -12,19 +12,17 @@ output_nodes = 1
 # DATA generation for 3.2.1
 mA = [1.0, 0.5]
 mB = [0.2, 0.3]
-sigmaA = 0.1
-sigmaB = 0.1
-data_points = 100
+sigmaA = 0.3
+sigmaB = 0.3
+n = 200
 n_classes = 2
 
-
-# WEIGHT
 
 
 def generate_weights(X):
     # Weight, W
     W = np.random.normal(1, 0.5, (hidden_nodes, X.shape[0]))
-
+    print(hidden_nodes)
     # Weight, V
     V = np.random.normal(1, 0.5, (output_nodes, hidden_nodes + 1))
     return W, V
@@ -33,30 +31,37 @@ def generate_weights(X):
 def generate_io_matrix(useBias=True):
     # np.random.seed(1000)
 
-    X = np.zeros([features + 1, data_points * 2])
+    X = np.zeros([features + 1, n * 2])
 
     # A
-    X[0, :data_points] = np.concatenate((np.random.normal(2, 1, int(data_points / 2)) * sigmaA - mA[0],
-                                         np.random.normal(2, 1, int(data_points / 2)) * sigmaA + mA[0]))
-    X[1, :data_points] = np.random.normal(0, 1, data_points) * sigmaA + mA[1]
-    X[2, :data_points] = 1 if useBias else 0
+    X[0, :n] = np.concatenate((np.random.normal(2, 1, int(n / 2)) * sigmaA - mA[0],
+                                         np.random.normal(2, 1, int(n / 2)) * sigmaA + mA[0]))
+    
+    np.random.shuffle(X[0, :n])
+
+    X[1, :n] = np.random.normal(0, 1, n) * sigmaA + mA[1]
+    X[2, :n] = 1 if useBias else 0
 
     # B
-    X[0, data_points:] = np.random.normal(0, 1, data_points) * sigmaB + mB[0]
-    X[1, data_points:] = np.random.normal(0, 1, data_points) * sigmaB + mB[1]
-    X[2, data_points:] = 1 if useBias else 0
+    X[0, n:] = np.random.normal(0, 1, n) * sigmaB + mB[0]
+    X[1, n:] = np.random.normal(0, 1, n) * sigmaB + mB[1]
+    X[2, n:] = 1 if useBias else 0
 
     # T
-    T = np.zeros(data_points * 2)
-    T[:data_points] = 1
-    T[data_points:] = -1
+    T = np.zeros(n * 2)
+    T[:n] = 1
+    T[n:] = -1
     T = phi(T)
+
+    #idx = np.random.permutation(n)
+    
+
     return X, T
 
 
 def generate_io_encoder_matrix():
-    rand = np.random.randint(features, size=data_points)
-    X = np.ones([data_points, features]) * -1
+    rand = np.random.randint(features, size=n)
+    X = np.ones([n, features]) * -1
     for i, x in enumerate(X):
         x[rand[i]] = 1
     print(X)
@@ -93,6 +98,8 @@ def phi_prim(x):
 def forward_pass(X, W, V):
     # X = np.concatenate((X, np.ones((1, X.shape[1]))))     ## Eventuellt senare
     # h_in = W @ np.concatenate((X, np.ones((1, X.shape[1])))) ###viktigt
+    #print(X.shape)
+    #print(W.shape)
     h_in = W @ X
     # h_out = np.array([phi(h_in)], [1]*X.shape[1])         ### Kommer ge error
     h_out = np.concatenate((phi(h_in), np.ones((1, X.shape[1]))))
@@ -137,24 +144,27 @@ def weight_update(delta_h, delta_o, X, W, H, V, use_momentum=False, dw=0, dv=0):
         return W, V, dw, dv
 
 
-def calculate_accuracy(o_out):
+def calculate_accuracy(o_out, testT):
     counter = 0
-    for i in range(0, data_points, 1):
-        if o_out[0, i] <= 0:
+    for i in range(len(testT)):
+        if testT[i] > 0 and o_out[0, i] > 0:
             counter += 1
-    for i in range(data_points, data_points * 2, 1):
-        if o_out[0, i] > 0:
+        elif testT[i] < 0 and o_out[0, i] < 0:
             counter += 1
+
     if counter == 0:
         return 1.0
     else:
-        return (data_points * 2 - counter) / (data_points * 2)
+        return 1.0 - ((len(testT) - counter) / len(testT))
 
 
-def two_layer_perceptron(X, T, W, V, n_epoch, x, y, use_momentum=False):
+def two_layer_perceptron(X, T, W, V, n_epoch, testX, testT, use_momentum=False, use_bell=False, x=None, y=None):
     mse = np.zeros(n_epoch)
     dw = 0
     dv = 0
+    test_accuracy = np.zeros(n_epoch)
+    train_accuracy = np.zeros(n_epoch)
+    
     for i in range(n_epoch):
         o_out, h_out = forward_pass(X, W, V)
         delta_h, delta_o = backward_pass(o_out, h_out, T, V)
@@ -162,24 +172,72 @@ def two_layer_perceptron(X, T, W, V, n_epoch, x, y, use_momentum=False):
             W, V, dw, dv = weight_update(delta_h, delta_o, X, W, h_out, V)
         else:
             W, V, dw, dv = weight_update(delta_h, delta_o, X, W, h_out, V, True, dw, dv)
-        mse[i] = MSE(T, o_out)
-        #plot_bell_3d(o_out, x, y)
+        #mse[i] = MSE(testT, o_out)
+        #if use_bell:
+            #if (i%100) == 0:
+                #plot_bell_3d(o_out, x, y)
         #print(mse[i])
+        o_out_test, h_out_test = forward_pass(testX, W, V)
+        #print(testT.shape, o_out_test.shape)
+        #print(trainT.shape, o_out_test.shape)
 
-    return W, V, dw, dv, o_out, h_out, mse
+        test_accuracy[i] = MSE(testT, o_out_test)       #calculate_accuracy(o_out_test, testT)
+        train_accuracy[i] = MSE(T, o_out)          #calculate_accuracy(o_out, T)
 
+
+    #o_out_test, h_out_test = forward_pass(testX, W, V)
+    #last_error = MSE(testT, o_out_test)
+    #last_error = calculate_accuracy(o_out_test, testT)
+    return W, V, dw, dv, o_out, h_out, train_accuracy, test_accuracy
+
+
+def plot(a1, a2, b1, b2):
+    plt.scatter(a1, a2, color="red")
+    plt.scatter(b1, b2, color="blue")
+    plt.show()
 
 def plot_data_points(X):
     plt.ylim(top=100, bottom=-100)
     plt.xlim(right=100, left=-100)
-    classAx = X[0, :data_points]
-    classAy = X[1, :data_points]
-    classBx = X[0, data_points:]
-    classBy = X[1, data_points:]
+    classAx = X[0, :n]
+    classAy = X[1, :n]
+    classBx = X[0, n:]
+    classBy = X[1, n:]
     plt.scatter(classAx, classAy, color="red")
     plt.scatter(classBx, classBy, color="blue")
+    plt.xlabel("X1-Feature")
+    plt.ylabel("X2-Feature")
+    plt.title("Classification of linearly non-separable data, eta=" + str(eta))
     plt.grid()
     #plt.show()
+
+def splitData(InputData, TargetData, A_Ratio=0.0, B_Ratio=0.0):
+
+    testA = InputData[:, :int(n * A_Ratio)]
+    trainingA = InputData[:, int(n * A_Ratio):n]
+    testB = InputData[:, n: n + int((B_Ratio * n))]
+    trainingB = InputData[:, n + int((B_Ratio * n)): n * 2]
+
+    testAT = TargetData[0:int(A_Ratio * n):1]
+    trainingAT = TargetData[int(A_Ratio * n):n:1]
+    testBT = TargetData[n:n + int(B_Ratio * n):1]
+    trainingBT = TargetData[n + int(B_Ratio * n):2 * n:1]
+
+    if B_Ratio == 0:
+        testX = testA
+        testT = testAT
+    elif A_Ratio == 0:
+        testX = testB
+        testT = testBT
+    else:
+        testX = np.concatenate((testA, testB), axis=1)
+        testT = np.concatenate((testAT, testBT))
+
+    trainingX = np.concatenate((trainingA, trainingB), axis=1)
+    trainingT = np.concatenate((trainingAT, trainingBT))
+
+    #plot(testA[0], testA[1], testB[0], testB[1])
+    return testX, testT, trainingX, trainingT
 
 
 def plot_mse(mse_array):
@@ -191,7 +249,7 @@ def plot_mse(mse_array):
     plt.ylabel("Error rate")
     plt.title("Mean Square Error for every epoch with eta = " + str(eta))  ##ändra till bättre, tack
     plt.plot(x, y, color='green', label="Mean Square Error")
-    plt.legend(loc="upper right")
+    plt.legend(loc="lower left")
     plt.show()
 
 
@@ -218,19 +276,81 @@ def plot_bell(output, x, y):
     plt.contourf(xx, yy, zz)
     plt.show()
 
-def plot_bell_3d(output, x, y):
+def plot_bell_3d(output, x, y, use_ratio=False, ratio=0.0):
     ax = plt.axes(projection="3d")
+    print(output.shape, x.shape, y.shape)
     xx, yy = np.meshgrid(x, y)
     zz = np.reshape(output, xx.shape)
     ax.plot_surface(xx, yy, zz)
-    plt.show()
-    #plt.pause(0.01)
+    #plt.show()
+    plt.pause(0.0001)
     #plt.waitforbuttonpress()
 
+def plot_accuracy(train_acc, test_acc):
+    plt.ylim(top=1, bottom=0)
+    plt.xlim(right=n_epochs, left=0)
+    epochs = np.arange(0, n_epochs)
+    plt.xlabel("Number of Epochs")
+    plt.ylabel("Mean Square Error")
+    plt.title("Mean Square Error of the function approximation \nwith " + str(hidden_nodes) + " hidden nodes and eta =" + str(eta))  ##ändra till bättre, tack
+    plt.plot(epochs, train_acc, color='green', label="MSE change for train")
+    plt.plot(epochs, test_acc, color='red', label="MSE change for test")
+
+    plt.legend(loc="upper right")
+    plt.show()
+
+def plot_for_nodes(train, test, nodes_used):
+    plt.ylim(top=1, bottom=0)
+    plt.xlim(right=24, left=0)
+    nodes = np.arange(0, nodes_used)
+    print(nodes.shape)
+    print(train.shape)
+    print(test.shape)
+    plt.xlabel("Size of hidden layer")
+    plt.ylabel("Mean Square Error")
+    plt.title("Mean Square Error for the function approximation")# \nwith " + str(hidden_nodes) + " hidden nodes and eta =" + str(eta))  ##ändra till bättre, tack
+    plt.plot(nodes, train, color='green', label="MSE change for train")
+    plt.plot(nodes, test, color='red', label="MSE change for test")
+
+    plt.legend(loc="upper right")
+    plt.show()
+
+
+def split_bell_data(X, T, test_ratio=0.2):
+    points = int(X.shape[1])
+    test_points = int(X.shape[1]*test_ratio)
+    testX = np.zeros([X.shape[0], test_points])
+    testT = np.zeros([T.shape[0], test_points])
+    trainX = np.zeros([X.shape[0], int(points-test_points)])
+    trainT = np.zeros([T.shape[0], int(points-test_points)])
+
+    for i in range(test_points):
+        testX[:, i] = X[:, i]
+        testT[0, i] = T[0, i]
+    
+    for i in range(points-test_points):
+        trainX[:, i] = X[:, i+test_points]
+        trainT[0, i] = T[0, i+test_points]
+
+    return trainX, trainT, testX, testT
 
 """ TASK 3.2.1 """
+#iterations = 25
+#for i in range(1, iterations+1):
+    #hidden_nodes = i
 #X, T = generate_io_matrix()
 #W, V = generate_weights(X)
+#plot_data_points(X)
+#plt.show()
+#testX, testT, trainingX, trainingT = splitData(X, T, 0.2, 0.2)
+#print(testX.shape)
+#print(testX.shape)
+#W, V, dw, dv, o_out, h_out, train_accuracy, test_accuracy = two_layer_perceptron(trainingX, trainingT, W, V, n_epochs, testT, testX)
+#print(error)
+#print("Iteration " + str(i) + " done.")
+
+#print(test_accuracy)
+#plot_accuracy(train_accuracy, test_accuracy)
 
 """ TASK 3.2.2 """
 #X = generate_io_encoder_matrix()
@@ -238,28 +358,45 @@ def plot_bell_3d(output, x, y):
 
 """ TASK 3.2.3 """
 #print(gauss_bell_function(1,1))
+"""
+iterations = 2
+train_acc = np.zeros(iterations+1)
+test_acc = np.zeros(iterations+1)
+for i in range(1, iterations+1):
+    hidden_nodes = i
+    X, T, x, y = generate_bell_data()
+    W, V = generate_weights(X)
+    trainX, trainT, testX, testT = split_bell_data(X, T, 0.2)
+
+    
+    W, V, dw, dv, o_out, h_out, train_accuracy, test_accuracy = two_layer_perceptron(trainX, trainT, W, V, n_epochs, testX, testT, False, True, x, y)
+    train_acc[i-1] = train_accuracy
+    test_acc[i-1] = test_accuracy
+"""
 X, T, x, y = generate_bell_data()
 W, V = generate_weights(X)
-
-""" Training """
-W, V, dw, dv, o_out, h_out, mse_array = two_layer_perceptron(X, T, W, V, n_epochs, x, y, True)
+trainX, trainT, testX, testT = split_bell_data(X, T, 0.2)
+W, V, dw, dv, o_out, h_out, train_accuracy, test_accuracy = two_layer_perceptron(trainX, trainT, W, V, n_epochs, testX, testT, False, True, x, y)
 #print(o_out)
+plot_accuracy(train_accuracy, test_accuracy)
+#plot_for_nodes(train_acc, test_acc, iterations+1)
 
 """ Plotting """
 #plot_data_points(X)
 #for w in W:
-#    plotLine(w)
-#plt.grid()
-#plt.show()
+    #plotLine(w)
+    #break
+#plt.legend(loc="lower left")
 #plt.grid()
 #plt.show()
 #plot_mse(mse_array)
-print("-------O--------")
-print(o_out)
-print("-------T--------")
-print(T)
-print("------MSE----------")
-print(mse_array)
-plot_bell(o_out, x, y)
-plot_bell_3d(o_out, x, y)
+#print("-------O--------")
+#print(o_out)
+#print("-------T--------")
+#print(T)
+#print("------MSE----------")
+#print(mse_array)
+#plot_bell(o_out, x, y)
+#plot_bell_3d(o_out, x, y)
+#plt.show()
 
