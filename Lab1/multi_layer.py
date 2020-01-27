fd53da2aef83a84a21fa6cb9532c1e21cc6ae22a
Taoudi <taoudi128@gmail.com>
2020-01-27 01:03:46 +0100

    :rage:

--- Lab1/multi_layer.py
+++ Lab1/multi_layer.py
@@ -8,6 +8,7 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import BatchNormalization
 
 from matplotlib import pyplot
+import time
 
 lower = 301
 higher = 1501
@@ -16,14 +17,16 @@ points = 1200
 test_points = 200
 training_points = 1000
 features = 5
-hidden_nodes = 300
+hidden_nodes2 = 300
+hidden_nodes1 = 50
+
 output_nodes = 1
 mom = 0.99
 eta = 0.01
 epochs = 1000
 bs = 32
-iterations = 10
-sd = 0.03
+iterations = 3
+sd = 0.09
 
 
 def generate_io_data(x):
@@ -71,16 +74,15 @@ def mackey_glass(t_iter):
     return x
 
 
-def network(X_training, Y_training, X_test, plot=True, regularization=True):
+def network(X_training, Y_training, X_test, plot=True, regularization=True, hidden_nodes=hidden_nodes2):
     # Compiling
     model = Sequential()
     model.add(Dense(3, input_shape=(features,)))
-    if regularization:
-        model.add(Dropout(0.2))
-        #model.add(Dense(hidden_nodes, kernel_regularizer=l2(l=0.1)))
+    #model.add(Dropout(0.2))
+    # model.add(Dense(hidden_nodes, kernel_regularizer=l2(l=0.1)))
 
-    model.add(Dense(hidden_nodes))
-    model.add(Dropout(0.2))
+    model.add(Dense(hidden_nodes1))
+    #model.add(Dropout(0.2))
     model.add(Dense(hidden_nodes))
     model.add(Dense(output_nodes))
     sgd = SGD(learning_rate=eta, momentum=mom)
@@ -91,8 +93,11 @@ def network(X_training, Y_training, X_test, plot=True, regularization=True):
     es = EarlyStopping(monitor='val_loss', patience=10)
 
     # verbose = 2
+    start_time = time.time()
     history = model.fit(X_training, Y_training, epochs=epochs, batch_size=bs, verbose=0,
                         validation_split=0.2, callbacks=[es])
+    elapsed_time = time.time() - start_time
+
     yhat = model.predict(X_test)
     weights, biases = model.layers[0].get_weights()
 
@@ -106,7 +111,7 @@ def network(X_training, Y_training, X_test, plot=True, regularization=True):
         pyplot.show()
 
     # print(yhat)
-    return yhat, history.history['val_loss'][len(history.history['val_loss']) - 1], weights
+    return yhat, history.history['val_loss'][len(history.history['val_loss']) - 1], weights, elapsed_time
 
 
 def MSE(T, y):
@@ -114,7 +119,7 @@ def MSE(T, y):
 
 
 def plot_histo(weights):
-    x= np.linspace(1, features, features)
+    x = np.linspace(1, features, features)
     y = weights
     print(weights.T[0])
     print(x)
@@ -125,11 +130,11 @@ def plot_histo(weights):
 
 
 def plot_time_series(training_T, test_T, y_predicted=None):
-    #x1 = np.linspace(lower, higher - test_points, training_points)
-    x1 = np.arange(lower,higher-test_points, 1)
-    x2 = np.arange(higher-test_points,higher, 1)
+    # x1 = np.linspace(lower, higher - test_points, training_points)
+    x1 = np.arange(lower, higher - test_points, 1)
+    x2 = np.arange(higher - test_points, higher, 1)
 
-    #x2 = np.linspace(higher - test_points, higher, test_points)
+    # x2 = np.linspace(higher - test_points, higher, test_points)
     print(x1)
     print(x2)
     pyplot.plot(x1, training_T, color='green')
@@ -149,10 +154,29 @@ def averageMSE(training, training_T, test):
     while i < iterations:
         i += 1
         print(i)
-        y_predicted, error, weight = network(training.T, training_T, test.T, False, False)
-        #errors.append(MSE(test_T, y_predicted))
+        y_predicted, error, weight = network(training.T, training_T, test.T, False, True)
+        # errors.append(MSE(test_T, y_predicted))
         errors.append(error)
     print(np.mean(np.array(errors)))
+
+
+def takeTime(X_training, Y_training, X_test):
+    i = 0
+    iterations = 10
+    times = []
+    nodes = []
+    while i < iterations:
+        i += 1
+        hidden_nodes = i * 100
+        nodes.append(hidden_nodes)
+        x,y,z, time = network(X_training.T, Y_training, X_test.T, False, True, hidden_nodes)
+        print(time)
+        times.append(time)
+    pyplot.plot(nodes, times)
+    pyplot.title("Time elapsed depending on hidden nodes")
+    pyplot.xlabel("Amount of hidden nodes in 2nd layer")
+    pyplot.ylabel("Time Elapsed")
+    pyplot.show()
 
 
 x = mackey_glass(higher + predict)
@@ -160,12 +184,13 @@ inputs, output = generate_io_data(x)
 
 training, test, training_T, test_T = split_data(inputs, output)
 
-averageMSE(training,training_T,test)
-y_predicted, error, weights = network(training.T, training_T, test.T, False, False)
-#print(weights)
-#plot_histo(weights)
-#plot_time_series(training_T, test_T, y_predicted)
+# averageMSE(training,training_T,test)
+# y_predicted, error, weights = network(training.T, training_T, test.T, False, True)
+# print(weights)
+# plot_histo(weights)
+# plot_time_series(training_T, test_T, y_predicted)
 
+takeTime(training, training_T, test)
 # print(inputs)
 # print(output)
 # print(inputs)
