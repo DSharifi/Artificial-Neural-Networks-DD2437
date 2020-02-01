32b104d4976c6d455b0c1aaf5165638db531e636
Taoudi <Taoudi128@hotmail.com>
2020-02-01 19:44:28 +0100

    :)

--- Lab2/Competitive.py
+++ Lab2/Competitive.py
@@ -4,9 +4,34 @@ import matplotlib.pyplot as plt
 
 datapoints = 63
 feature = 1
-nodes = 30
+nodes = 90
 sigma = 1
+amt_learn = 1
+eta = 0.2
 noise = True
+winners = np.zeros(nodes)
+
+
+def find_X_closest(x, W, amt=amt_learn):
+    """
+    :param x: current datapoint
+    :param W: W Matrix
+    :return: Column index for the closest weight
+    """
+    # closest_list = np.ones(amt) * np.inf
+    # closest_list = sorted(closest_list, key=lambda row: row[0])
+    distances = np.zeros(W.shape[0])
+    for i, w in enumerate(W):
+        distances[i] = np.linalg.norm(x - w)
+    weights = []
+    for i in range(amt):
+        if i >= nodes:
+            break
+        index = np.argmin(distances)
+        winners[index] += 1
+        weights.append(index)
+        distances[index] = np.inf
+    return weights
 
 
 def find_closest(x, W):
@@ -19,34 +44,37 @@ def find_closest(x, W):
     for i, w in enumerate(W):
         if np.linalg.norm(x - w) < np.linalg.norm(x - W[int(closest)]):
             closest = i
-    return closest
+    return np.array([closest])
 
 
-def delta_w(x, w, eta=0.5):
+def delta_w(x, w, eta):
+    # print(x-w)
     return eta * (x - w)
 
 
 def train_x(x, W):
-    w_index = find_closest(x, W)
-    # print(W[w_index])
-    W[w_index] += delta_w(x, W[w_index])
-    # print(W[w_index])
+    w_index = find_X_closest(x, W)
+    for i, id in enumerate(w_index):
+        # print("id: " + str(id))
+        # print(W[id])
+        W[id] += delta_w(x, W[id], eta / (i + 1))
+        # print(W[id])
+    # print("-----------------")
     return W
 
 
 def init_weights():
-    return np.random.rand(nodes)
+    return np.random.rand(nodes, feature)
 
 
 def gaussian_rbf(x1, x2, sigma):
-    return np.exp(-(x1 - x2) ** 2 / 2 * (sigma ** 2))
+    return np.exp(-np.linalg.norm(x1 - x2) ** 2 / 2 * (sigma ** 2))
 
 
 def predict_x(weights, x_values):
     predictions = np.zeros(nodes)  # will be features x hidden_nodes
     for i in range(nodes):
         predictions[i] = gaussian_rbf(x_values, weights[i], sigma)
-        print(predictions[i])
     return np.sum(predictions)
 
 
@@ -79,48 +107,64 @@ def plot_error(nodes, errors):
     plt.show()
 
 
-def network(weights, training, testing, targets, plot=False):
+def network(weights, training, testing, targets_training, targets_test, plot=False):
     phi = generate_phi_matrix(weights, training)
-    W = least_squares(phi, targets)
+    W = least_squares(phi, targets_training)
     phi_test_matrix = generate_phi_matrix(weights, testing)
-    total_error = np.abs(np.subtract(phi_test_matrix @ W, targets)).mean()
-    print(total_error)
+    total_error = np.abs(np.subtract(phi_test_matrix @ W, targets_test)).mean()
     if plot:
         plt.plot(testing, phi_test_matrix @ W, label="Approximate")
-        plt.plot(testing, targets, label="True")
-        plt.scatter(weights, np.zeros(nodes), label="weights")
+        plt.plot(testing, targets_test, label="True")
+        # plt.scatter(weights.T[0], weights.T[1], label="weights")
         plt.legend()
-        plt.title("RBF Network with Competitive Learning \n  hidden nodes: " + str(nodes) + ", sigma: " + str(sigma))
+        plt.title("RBF Network with Competitive Learning \n  hidden nodes: " + str(nodes) + ", sigma: " + str(
+            sigma) + ", learning rate: " + str(eta) + ", Amount of winners: " + str(amt_learn))
         plt.grid()
         plt.show()
     return total_error
 
 
 def competitive_learning(training):
-    iterations = 20
+    iterations = 100
     W = init_weights()
     for i in range(0, iterations):
         W = train(W, training)
     return W
 
 
-def error_testing():
+def error_testing(training, testing, target, targety):
     global nodes
-    training, testing, sin_target, square_target = r.generate_data(noise)
     errors = []
     amt_nodes = []
-    for i in range(2, nodes):
-        print(i)
+    for i in range(5, nodes):
         nodes = i
         weights = competitive_learning(training)
         amt_nodes.append(i)
-        errors.append(network(weights, training, testing, sin_target))
+        errors.append(network(weights, training, testing, target, targety))
     plot_error(amt_nodes, errors)
 
 
-training, testing, sin_target, square_target = r.generate_data(noise)
-weights = competitive_learning(training)
-network(weights, training, testing, sin_target, True)
-#error_testing()
+def read_file_data(filename):
+    file = "data_lab2/" + filename
+
+    file = open(file, "r")
+    list = []
+    templist = []
+    for i, line in enumerate(file.read().split()):
+        if i % 4 == 0 and i != 0:
+            list.append(templist)
+            templist = []
+        templist.append(line)
+    list.append(templist)
+    training = np.asfarray(np.array(list), float)
+    training_X, training_Y = np.hsplit(training, 2)
+    return training_X, training_Y
 
 
+if __name__ == "__main__":
+    # training_x, training_y = read_file_data("ballist.dat")
+    # test_x, test_y = read_file_data("balltest.dat")
+    training_x, testing_x, sin2x_target, square2x_target, sintarget_y = r.generate_data(False)
+    error_testing(training_x, testing_x, sin2x_target, sintarget_y)
+    # weights = competitive_learning(training_x)
+    # network(weights, training_x, testing_x, sin2x_target, sintarget_y, True)
